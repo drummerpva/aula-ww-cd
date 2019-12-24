eecd9e66b6b5faa524a0336a2ac6e751079300ae
Douglas Poma <pia_da_lan@yahoo.com.br>
2019-12-24 13:49:21 -0300

    Primeiro Commit

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const app = express();
 
+const port = process.env.PORT || 3333;
+
 app.get("/", (req, res) => {
   res.send("Olá Express");
 });
 
-app.listen(3333, () => {
+app.listen(port, () => {
   console.log("Running Server");
 });
